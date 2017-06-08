7ad2a48fd4899f8d72ee1bdc6902975eee152d8d
Dikran Hachikyan <dhachikian@expert-bg.org>
2017-06-08 15:35:42 +0300

    Input Decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+@Component({
+	selector:'child-component',
+	template:`<p>{{msg}}</p>`
+})
+export class ChildInputComponent{
+	@Input() private msg:string;
+}
 
 @Component({
 	selector:'demo',
-	template:`<h1>{{title}}</h1>`
+	template:`<h1>{{title}}</h1>
+			 <child-component [msg]="message"></child-component>`
 })
 export class AppComponent{
 	private title:string = 'Parent Component';
+	private message:string = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.';
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
+import {AppComponent, ChildInputComponent} from './app.component';
 
 
 @NgModule({
 	imports:[BrowserModule],
-	declarations:[AppComponent],
+	declarations:[AppComponent,ChildInputComponent],
 	bootstrap:[AppComponent]
 })
 export class AppModule {
