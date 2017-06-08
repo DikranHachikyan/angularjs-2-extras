f562d050c7d7d94fc1fb53c5323162fb87face0c
Dikran Hachikyan <dhachikian@expert-bg.org>
2017-06-08 15:56:41 +0300

    Event Emitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component} from '@angular/core';
+import { Component, EventEmitter,Output} from '@angular/core';
 
 @Component({
 	selector:'child-component',
 	inputs:['msg'],
-	template:`<p (click)="show(msg)">{{msg}}</p>`
+	template:`<p (click)="show(msg)">{{msg}}</p>
+			 <button (click)="sendEvent()">Send</button>`
 })
 export class ChildInputComponent{
+	@Output() send = new EventEmitter<any>();
+	private data:{} = { quantity:2, title:'Mastering AngularJS', price:10.20};
+	sendEvent():void{
+		this.send.emit( this.data);
+	}
 	show(m:string):void{
 		console.log(`Message:${m}`);
 	}
@@ -14,10 +20,14 @@ export class ChildInputComponent{
 @Component({
 	selector:'demo',
 	template:`<h1>{{title}}</h1>
-			 <child-component [msg]="message"></child-component>`
+			 <child-component [msg]="message" (send)="showData($event)"></child-component>`
 })
 export class AppComponent{
 	private title:string = 'Parent Component';
 	private message:string = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.';
+
+	showData(data:any):void{
+		console.log(data);
+	}
 
 }
